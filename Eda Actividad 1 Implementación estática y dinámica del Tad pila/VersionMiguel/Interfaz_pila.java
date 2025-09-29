411399940a8fc079501549b7d0427cf5b2d11150
Kymdrus <73409820+Kymdrus@users.noreply.github.com>
2025-09-29 18:55:01 +0200

    Add files via upload

---
+++ Eda Actividad 1 Implementación estática y dinámica del Tad pila/VersionMiguel/Interfaz_pila.java
@@ -0,0 +1,8 @@
+public interface  Interfaz_pila <T>{
+    void push(T elemento);
+    T pop();
+    T top();
+    boolean isEmpty();
+    int size();
+    String toString();
+}
---
+++ Eda Actividad 1 Implementación estática y dinámica del Tad pila/VersionMiguel/PilaEstatica.java
@@ -0,0 +1,8 @@
+public class PilaEstatica implements  Interfaz_pila{
+  public PilaEstatica(){
+
+  }
+  public  pop(){
+
+  }
+}
