e71b842b2d008ae5c7e78cdebf2c16f54d694417
Alevi700 <Alejandro.Vicioso1@alu.uclm.es>
2025-10-09 13:53:32 +0200

    Javadoc Main

--- Eda Actividad 1 Implementación estática y dinámica del Tad pila/VersionVicioso/Aplicacion.java
+++ Eda Actividad 1 Implementación estática y dinámica del Tad pila/VersionVicioso/Aplicacion.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Aplicacion {
+    /**Este es el método main, en el que pedimos al usuario que introduzca un número y la base a la que quiere convertirlo,
+     * así como el tipo de pila que quiere usar (estática o dinámica). Luego se llama al método Cambiar_base para realizar
+     * la conversión y se muestra el resultado por pantalla.
+     * */
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Introduzca el numero a convertir: \n");
@@ -20,16 +24,24 @@ public class Aplicacion {
                 PilaEstatica<Integer> pila = new PilaEstatica<>(10);
                 System.out.print("El numero " + numero + " en base " + base + " es: " + Cambiar_base(numero, base, pila) + "\n");
                 valido = false;
+                
             }
             else{
                 System.out.print("Opcion no valida, introduzca D o d (Para dinamica) o S o s (Para estatica) \n");
                 valido = true;
             }
-            
+            sc.close();
         }
 
     }
 
+/**Este método realiza de forma recursiva el cambio de base usando una pila auxiliar. Nuestro caso base consiste
+ * en que el cociente de la división sea menor que la base, en ese caso simplemente apilamos el resto y el cociente
+ * y devolvemos la pila convertida a cadena de caracteres usando toString. En el caso recursivo apilamos el resto y
+ * se actializa el dividendo al cociente, llamando de nuevo al método.
+ * Detalle importante: el método recibe como parámetro una pila que puede ser estática o dinámica, ya que ambas
+ * implementan la misma interfaz.
+ */
     public static String Cambiar_base(String numero, int base, Interfaz_pila<Integer> pila){
         
         int dividendo = Integer.parseInt(numero);
